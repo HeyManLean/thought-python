6864a8ba07c9da285c0f7a51efc878111e4499fd
linliangbin <linliangbin@91prize.com>
2019-02-25 13:16:44 +0800

    improve try_parse

--- try_urllib/try_parse.py
+++ try_urllib/try_parse.py
@@ -39,7 +39,7 @@ def test_urlparse():
     print(params_dict)
     # [('offset', '20'), ('limit', '10')]
 
-    query_string = parse.urlencode(params_dict)
+    query_string = parse.urlencode(params_dict)  # 将 key-value 形式生成 query 串
     print(query_string)
     # offset=20&limit=10
 
@@ -56,7 +56,34 @@ def test_urlparse():
     # DefragResult(url='https://www.example.com/posts;python?offset=20&limit=10', fragment='文章')
 
 
+def test_quote():
+    """测试 url 转义编码 - for non-ASCII"""
+    url = 'https://www.example.com/posts;python?offset=20&limit=10#文章'
+    quote_url = parse.quote(url)
+    print(quote_url)  # safe 默认为 '/', 所以没转义
+    # https%3A//www.example.com/posts%3Bpython%3Foffset%3D20%26limit%3D10%23%E6%96%87%E7%AB%A0
+    quote_url = parse.quote(url, safe='')
+    print(quote_url)
+    # https%3A%2F%2Fwww.example.com%2Fposts%3Bpython%3Foffset%3D20%26limit%3D10%23%E6%96%87%E7%AB%A0
+    
+    plus_url = 'https://www.example.com/posts; python?offset=20&limit=10# 文章'
+    quote_plus_url = parse.quote_plus(plus_url)  # 默认 safe 为 '', 且可以编码空字符 ' '
+    print(quote_plus_url)
+    # https%3A%2F%2Fwww.example.com%2Fposts%3B+python%3Foffset%3D20%26limit%3D10%23+%E6%96%87%E7%AB%A0
+
+    unquote_url = parse.unquote(quote_url)
+    print(unquote_url)
+    # https://www.example.com/posts;python?offset=20&limit=10#文章
+
+    unquote_url = parse.unquote(quote_plus_url)  # 不支持空字符 ' '
+    print(unquote_url)
+    # https://www.example.com/posts;+python?offset=20&limit=10#+文章
+
+    unquote_plus_url = parse.unquote_plus(quote_plus_url)
+    print(unquote_plus_url)
+    # https://www.example.com/posts; python?offset=20&limit=10# 文章
 
 
 if __name__ == '__main__':
     test_urlparse()
+    test_quote()
